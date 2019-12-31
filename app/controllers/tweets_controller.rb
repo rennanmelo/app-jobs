2fe384e82eb2483296ef96c06e7872582485d479
Rennan Melo <rennan.melo11@gmail.com>
2019-12-31 14:22:53 -0200

    Connect to the twitter api and get the tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,15 @@
 class TweetsController < ApplicationController
   def index
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = "7qqGDH7sx46zexxmoko3QRwmJ"
+      config.consumer_secret     = "afxajxGZt5nzkRQm5V0tosaRtqwQCKpslm8lzxCB2bAJSxXa19"
+      config.access_token        = "1030183222909526016-wMcHUrXSjXn8RUOnsIk6MT9kHObM59"
+      config.access_token_secret = "rTZ5qXrxQtKGSCjreewZvqybdHXrGLC9Z67H95EzzbCNW"
+    end
+
+    client.search('#rails #jobs -rt').each do |tweet|
+      puts "#{tweet.user.name} - #{tweet.text} - #{tweet.url} - #{tweet.created_at}"      
+      puts "----------------------------------"   
+    end
   end
 end
